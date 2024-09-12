fdde74ec990f6899bcfe58ba4a00fe1d4a162a10
TUNAGOD999 <122945109+TUNAGOD999@users.noreply.github.com>
2024-09-12 12:46:35 -0400

    Update index.js
    Calculator

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("Calculator");
+
+let num1 = prompt("what is your first number")
+let num2 = prompt("what is your second number")
+
+let operation = ("what operation are you using?")
+
+if (operation = "+") {
+    addition(num1 + num2)
+}
+if (operation = "-") {
+    subtraction(num1 - num2)
+}
+if (operation = "/") {
+    division(num1 / num2)
+}
+if (operation = "x") {
+    multiplication(num1 * num2)
+}
