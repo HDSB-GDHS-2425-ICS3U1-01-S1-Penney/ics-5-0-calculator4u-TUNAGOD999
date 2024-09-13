70a921426158a7010027f2007e47e61b8a46f4b0
TUNAGOD999 <122945109+TUNAGOD999@users.noreply.github.com>
2024-09-13 12:39:50 -0400

    Update index.js
    This is the final copy of the java script calculator, as it is done.

--- index.js
+++ index.js
@@ -1,16 +1,109 @@
 console.log("Calculator");
+let op = prompt("What operation are you going to use? +, -, x, /, squared, square root, working with triangles, unit converter");
 
-let num1 = prompt("what is your first number")
-let num2 = prompt("what is your second number")
 
-let operation = prompt(Number("what operation are you using? +, -, /, x"))
 
-if (operation = "+") {
-   console.log(num1 + num2)
-} else if (operation = "-") {
-    console.log(num1 - num2)
-} else if (operation = "/") {
-    console.log(num1 / num2)
-} else if (operation = "x") {
-    console.log(num1 * num2)
+if (op == "unit converter") {
+  let amount = parseFloat(prompt("What is the amount you would like to convert"))
+  let unitamount = prompt("what is your amount in? mm, cm, m, km?");
+  let unitinto = prompt("what unit do you want it converted into? mm, cm, m, km")
+  
+  if (unitamount == "km" && unitinto == "m") {
+    console.log(amount * 1000)
+    
+  } else  if (unitamount == "km"&& unitinto == "cm") {
+    console.log(amount * 100000)
+  
+  } else  if (unitamount == "km" && unitinto == "mm") {
+    console.log(amount * 1000000)
+  }
+  if (unitamount == "m" && unitinto == "km") {
+    
+    console.log(amount / 1000)
+  } else   if (unitamount == "m" && unitinto == "cm") {
+    
+    console.log(amount * 100)
+  } else   if (unitamount == "m" && unitinto == "mm") {
+    
+    console.log(amount * 1000)
+  } 
+ if (unitamount == "cm" && unitinto == "km") {
+    
+    console.log(amount / 100000)
+  } else   if (unitamount == "cm" && unitinto == "m") {
+    
+    console.log(amount / 100)
+  } else   if (unitamount == "cm" && unitinto == "mm") {
+    
+    console.log(amount * 10)
+  } 
+  if (unitamount == "mm" && unitinto == "km") {
+    
+    console.log(amount / 1000000)
+  } else   if (unitamount == "mm" && unitinto == "m") {
+    
+    console.log(amount / 1000)
+  } else   if (unitamount == "mm" && unitinto == "cm") {
+    
+    console.log(amount / 10)
+  } 
+  
+  
+} else if (op == "working with triangles") {
+  let triFunction = prompt("Would you like to use SIN, COS, or TAN to find the angle?");
+  if (triFunction == "SIN") {
+    let O = parseFloat(prompt("what is the length of the opposite side of the triangle"))
+    
+    let H = parseFloat(prompt("what is the length of the hypotenuse of the triangle"))
+  
+    const angle = Math.asin(O/H);
+    const degree = angle * (180 / Math.PI);
+    console.log(degree)
+    
+  } else if (triFunction == "COS") {
+    
+     let A = parseFloat(prompt("what is the length of the opposite side of the triangle"))
+    
+    let H = parseFloat(prompt("what is the length of the hypotenuse of the triangle"))
+    
+    const angle = Math.asin(A/H);
+    const degree = angle * (180 / Math.PI);
+    console.log(degree)
+    
+  } else if (triFunction == "TAN") {
+    
+      let O = parseFloat(prompt("what is the length of the opposite side of the triangle"))
+    
+    let A = parseFloat(prompt("what is the length of the hypotenuse of the triangle"))
+    
+     const angle = Math.asin(O/A);
+    const degree = angle * (180 / Math.PI);
+    console.log(degree)
+    
+  } else {
+    console.log("Invalid triangle function");
+  }
+} else if (op == "squared") {
+  let numSquared = parseFloat(prompt("What number do you want squared?"));
+  console.log(numSquared * numSquared);
+} else if (op == "square root") {
+  let numSquareRoot = parseFloat(
+    prompt("What number do you want square rooted?")
+  );
+  console.log(Math.sqrt(numSquareRoot));
+} else {
+  let num1 = parseFloat(prompt("What is your first number"));
+  let num2 = parseFloat(prompt("What is your second number"));
+  if (op == "+") {
+    console.log(num1 + num2);
+  } else if (op == "-") {
+    console.log(num1 - num2);
+  } else if (op == "x") {
+    console.log(num1 * num2);
+  } else if (op == "/") {
+    console.log(num1 / num2);
+  } else {
+    console.log("Invalid operation");
+  }
 }
+
