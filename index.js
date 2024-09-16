2ef62c539737c77805631f3cce8a84c23d4d6b83
TUNAGOD999 <122945109+TUNAGOD999@users.noreply.github.com>
2024-09-16 11:59:55 -0400

    Update index.js
    Final final code for calculator.

--- index.js
+++ index.js
@@ -1,89 +1,96 @@
+// Shayan Imran
+// Sept 16, 2024
+// Code Calculator
 console.log("Calculator");
-let op = prompt("What operation are you going to use? +, -, x, /, squared, square root, working with triangles, unit converter");
 
-
+let op = prompt(
+  "What operation are you going to use? +, -, x, /, squared, square root, working with triangles, unit converter"
+); // Asks what you want to do
 
 if (op == "unit converter") {
-  let amount = parseFloat(prompt("What is the amount you would like to convert"))
-  let unitamount = prompt("what is your amount in? mm, cm, m, km?");
-  let unitinto = prompt("what unit do you want it converted into? mm, cm, m, km")
-  
+  // code for unit converter
+  let amount = parseFloat(
+    prompt("What is the amount you would like to convert")
+  );
+  let unitamount = prompt("what is your amount in? mm, cm, m, km, C, F?");
+  let unitinto = prompt(
+    "what unit do you want it converted into? mm, cm, m, km,"
+  );
+
   if (unitamount == "km" && unitinto == "m") {
-    console.log(amount * 1000)
-    
-  } else  if (unitamount == "km"&& unitinto == "cm") {
-    console.log(amount * 100000)
-  
-  } else  if (unitamount == "km" && unitinto == "mm") {
-    console.log(amount * 1000000)
+    console.log(amount * 1000);
+  } else if (unitamount == "km" && unitinto == "cm") {
+    console.log(amount * 100000);
+  } else if (unitamount == "km" && unitinto == "mm") {
+    console.log(amount * 1000000);
   }
   if (unitamount == "m" && unitinto == "km") {
-    
-    console.log(amount / 1000)
-  } else   if (unitamount == "m" && unitinto == "cm") {
-    
-    console.log(amount * 100)
-  } else   if (unitamount == "m" && unitinto == "mm") {
-    
-    console.log(amount * 1000)
-  } 
- if (unitamount == "cm" && unitinto == "km") {
-    
-    console.log(amount / 100000)
-  } else   if (unitamount == "cm" && unitinto == "m") {
-    
-    console.log(amount / 100)
-  } else   if (unitamount == "cm" && unitinto == "mm") {
-    
-    console.log(amount * 10)
-  } 
+    console.log(amount / 1000);
+  } else if (unitamount == "m" && unitinto == "cm") {
+    console.log(amount * 100);
+  } else if (unitamount == "m" && unitinto == "mm") {
+    console.log(amount * 1000);
+  }
+  if (unitamount == "cm" && unitinto == "km") {
+    console.log(amount / 100000);
+  } else if (unitamount == "cm" && unitinto == "m") {
+    console.log(amount / 100);
+  } else if (unitamount == "cm" && unitinto == "mm") {
+    console.log(amount * 10);
+  }
   if (unitamount == "mm" && unitinto == "km") {
-    
-    console.log(amount / 1000000)
-  } else   if (unitamount == "mm" && unitinto == "m") {
-    
-    console.log(amount / 1000)
-  } else   if (unitamount == "mm" && unitinto == "cm") {
-    
-    console.log(amount / 10)
-  } 
-  
-  
+    console.log(amount / 1000000);
+  } else if (unitamount == "mm" && unitinto == "m") {
+    console.log(amount / 1000);
+  } else if (unitamount == "mm" && unitinto == "cm") {
+    console.log(amount / 10);
+  }
 } else if (op == "working with triangles") {
-  let triFunction = prompt("Would you like to use SIN, COS, or TAN to find the angle?");
+  //Code for triangles
+  let triFunction = prompt(
+    "Would you like to use SIN, COS, or TAN to find the angle?"
+  );
   if (triFunction == "SIN") {
-    let O = parseFloat(prompt("what is the length of the opposite side of the triangle"))
-    
-    let H = parseFloat(prompt("what is the length of the hypotenuse of the triangle"))
-  
-    const angle = Math.asin(O/H);
+    let O = parseFloat(
+      prompt("what is the length of the opposite side of the triangle")
+    );
+
+    let H = parseFloat(
+      prompt("what is the length of the hypotenuse of the triangle")
+    );
+
+    const angle = Math.asin(O / H);
     const degree = angle * (180 / Math.PI);
-    console.log(degree)
-    
+    console.log(degree);
   } else if (triFunction == "COS") {
-    
-     let A = parseFloat(prompt("what is the length of the opposite side of the triangle"))
-    
-    let H = parseFloat(prompt("what is the length of the hypotenuse of the triangle"))
-    
-    const angle = Math.asin(A/H);
+    let A = parseFloat(
+      prompt("what is the length of the opposite side of the triangle")
+    );
+
+    let H = parseFloat(
+      prompt("what is the length of the hypotenuse of the triangle")
+    );
+
+    const angle = Math.asin(A / H);
     const degree = angle * (180 / Math.PI);
-    console.log(degree)
-    
+    console.log(degree);
   } else if (triFunction == "TAN") {
-    
-      let O = parseFloat(prompt("what is the length of the opposite side of the triangle"))
-    
-    let A = parseFloat(prompt("what is the length of the hypotenuse of the triangle"))
-    
-     const angle = Math.asin(O/A);
+    let O = parseFloat(
+      prompt("what is the length of the opposite side of the triangle")
+    );
+
+    let A = parseFloat(
+      prompt("what is the length of the hypotenuse of the triangle")
+    );
+
+    const angle = Math.asin(O / A);
     const degree = angle * (180 / Math.PI);
-    console.log(degree)
-    
+    console.log(degree);
   } else {
     console.log("Invalid triangle function");
   }
 } else if (op == "squared") {
+  // code for square and square root
   let numSquared = parseFloat(prompt("What number do you want squared?"));
   console.log(numSquared * numSquared);
 } else if (op == "square root") {
@@ -92,6 +99,7 @@ if (op == "unit converter") {
   );
   console.log(Math.sqrt(numSquareRoot));
 } else {
+  //Code for plus, minus, divide, and multiply
   let num1 = parseFloat(prompt("What is your first number"));
   let num2 = parseFloat(prompt("What is your second number"));
   if (op == "+") {
@@ -106,4 +114,3 @@ if (op == "unit converter") {
     console.log("Invalid operation");
   }
 }
-
